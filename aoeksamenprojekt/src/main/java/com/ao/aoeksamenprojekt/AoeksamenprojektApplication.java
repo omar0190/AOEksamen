b89c58b6873bcdeacc46e9bc054dff7d867611ec
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-11-26 13:40:23 +0100

    spring

---
+++ aoeksamenprojekt/src/main/java/com/ao/aoeksamenprojekt/AoeksamenprojektApplication.java
@@ -0,0 +1,13 @@
+package com.ao.aoeksamenprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoeksamenprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoeksamenprojektApplication.class, args);
+	}
+
+}
---
+++ aoeksamenprojekt/src/test/java/com/ao/aoeksamenprojekt/AoeksamenprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ao.aoeksamenprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoeksamenprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
