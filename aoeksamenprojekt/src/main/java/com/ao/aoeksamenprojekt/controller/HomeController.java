93705fe28a91d402a3c5485bdbccf10792c0e6ff
omar0190 <61865216+omar0190@users.noreply.github.com>
2020-11-27 00:34:25 +0100

    b

--- aoeksamenprojekt/src/main/java/com/ao/aoeksamenprojekt/controller/HomeController.java
+++ aoeksamenprojekt/src/main/java/com/ao/aoeksamenprojekt/controller/HomeController.java
@@ -9,18 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-PositionServiceJPA positionServiceJPA;
 
-    public HomeController(PositionServiceJPA positionServiceJPA) {
-        this.positionServiceJPA = positionServiceJPA;
-    }
 
     @GetMapping ("/")
     public String index(){
-        Position p1 = new Position();
-        p1.setTitle("EtEllerAndet");
-        p1.setDescription("IkkeEtEllerAndetIgen");
-        positionServiceJPA.save(p1);
         return "index";
     }
 
