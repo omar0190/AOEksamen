59145c85a2bd899a674ecf851853ff02ebc2b41f
alix0731 <56973809+alix0731@users.noreply.github.com>
2020-12-08 12:34:45 +0100

    index

--- aoeksamenprojekt/src/main/java/com/ao/aoeksamenprojekt/controller/PositionController.java
+++ aoeksamenprojekt/src/main/java/com/ao/aoeksamenprojekt/controller/PositionController.java
@@ -18,23 +18,25 @@ public class PositionController {
     public PositionController(PositionServiceJPA positionServiceJPA) {
         this.positionServiceJPA = positionServiceJPA;
     }
+
     @GetMapping("/stillinger")
-    public String stillinger(Model model){
+    public String stillinger(Model model) {
         ArrayList<Position> list = positionServiceJPA.findAll();
         Position position1 = list.get(0);
         System.out.println(position1.getTitle());
-        model.addAttribute("stillinger",list);
+        model.addAttribute("stillinger", list);
         return "stillinger";
     }
-    @GetMapping("/details{id}")
-    public String showPositions(@PathVariable("id") int id, Model model){
-        Optional<Position> position1 = positionServiceJPA.findByID(id); // Optional, gør at man skal hente objektet selv, og hente den med get
-    if (position1.isPresent()){
-        model.addAttribute("title", position1.get().getTitle());
-        model.addAttribute("description",position1.get().getDescription());
-    }
 
-return "stillingsbeskrivelse";
+    @GetMapping("/details{id}")
+    public String showPositions(@PathVariable("id") int id, Model model) {
+        Optional<Position> position1 = positionServiceJPA.findByID(id); // Optional, gør at man skal hente objektet selv, og hente den med get
+        if (position1.isPresent()) {
+            model.addAttribute("title", position1.get().getTitle());
+            model.addAttribute("description", position1.get().getDescription());
+        }
+
+        return "stillingsbeskrivelse";
     }
 
 }
